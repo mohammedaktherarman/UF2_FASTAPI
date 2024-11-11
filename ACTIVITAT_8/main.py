a8a403809b082b5408834e4dd107f5ef078f88af
Arman Mohammed Akther <2023_arman.mohammed@iticbcn.cat>
2024-11-11 19:17:49 +0100

    Readme modificat y metode POST creat

--- ACTIVITAT_8/main.py
+++ ACTIVITAT_8/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get("/items/{item_id}")
 async def read_item(item_id):
     return {"item_id": item_id}
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    country: str 
+    day:str
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # UF2_FASTAPI
+
+<p>prova del metode get amb SWAGGER exercici 3<p>
+![alt text](image.png)
+
+<p>Resposta:<p>
+![alt text](image-1.png)
+
+<p>prova del metode get amb POSTMAN y la sortida exercici 4<p>
+![alt text](image-2.png)
+
+<p>mostrar un error 404 amb response exercici 6<p>
+
+
+
