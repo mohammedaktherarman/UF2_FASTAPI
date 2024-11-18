b022acc8ccaca38998127425d0cf45926a26216c
Arman Mohammed Akther <2023_arman.mohammed@iticbcn.cat>
2024-11-18 18:41:33 +0100

    documentació Body-Nested Models

--- ACTIVITAT_9/main.py
+++ ACTIVITAT_9/main.py
@@ -1,18 +1,18 @@
-from typing import Annotated
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Item(BaseModel):
     name: str
-    description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price: float = Field(gt=0, description="The price must be greater than zero")
+    description: str | None = None
+    price: float
     tax: float | None = None
+    tags: list = []
+
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # UF2_FASTAPI
 
-<p>Prova Body-Fields<p>
+<p>Prova Body-Nested Models<p>
+
+![alt text](/captures/image-1.png)
 
 ![alt text](/captures/image.png)
