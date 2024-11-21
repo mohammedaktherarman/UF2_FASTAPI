6cd509e6a422017ab725b69c6d4bf30d6313f161
Arman Mohammed Akther <2023_arman.mohammed@iticbcn.cat>
2024-11-21 21:08:29 +0100

    Punt 6 refet

--- ACTIVITAT_9/main.py
+++ ACTIVITAT_9/main.py
@@ -1,34 +1,9 @@
 from fastapi import FastAPI
-from connection import conexio
 from schema import User
-import psycopg2
+from read import read_users
 
 app = FastAPI()
 
 @app.get("/users/", response_model=list[User])
-def read_users():
-    try:
-        conn = conexio()
-        cursor = conn.cursor()
-        cursor.execute(" select * from users")
-        registros = cursor.fetchall()
-
-        users = []
-
-        for registro in registros:
-            user_dict = {
-                "user_id": registro[0],
-                "user_name": registro[1],
-                "user_surname": registro[2],
-                "user_age": registro[3],
-                "user_email": registro[4],
-            }
-            users.append(user_dict)
-
-        return users
-
-    except (Exception, psycopg2.Error) as error:
-        return {"error": str(error)}
-
-    finally:
-        conn.close()
+def get_users():
+    return read_users()
---
+++ ACTIVITAT_9/read.py
@@ -0,0 +1,31 @@
+from connection import conexio
+import psycopg2
+
+
+def read_users():
+    try:
+        conn = conexio()
+        cursor = conn.cursor()
+        cursor.execute("select * from USERS")
+        registres = cursor.fetchall()
+
+        users = []
+
+        for registre in registres:
+            user_dict = {
+                "user_id": registre[0],
+                "user_name": registre[1],
+                "user_surname": registre[2],
+                "user_age": registre[3],
+                "user_email": registre[4],
+            }
+            users.append(user_dict)
+
+        return users
+
+    except (Exception, psycopg2.Error) as error:
+        return {"error": str(error)}
+
+    finally:
+        if conn:
+            conn.close()
