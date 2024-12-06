34897a689b0d11f5d53f7148087917fea9c0ed4e
Arman Mohammed Akther <2023_arman.mohammed@iticbcn.cat>
2024-12-06 10:26:22 +0100

    Endpoint numero de intents fet

--- ACTIVITAT_11/db.py
+++ ACTIVITAT_11/db.py
@@ -3,12 +3,35 @@ from connection import db_client
 
 def pillafrase(Idioma: str):
     try:
-        conn = db_client()
+        conn = db_client() 
         cur = conn.cursor()
 
         cur.execute("SELECT frase FROM pantalla WHERE Idioma = %s", (Idioma,))
-        frase = cur.fetchall()
+        frase = cur.fetchall()  
+    finally:
+        conn.close()
+
+    return frase
+
+def intents(partida_id: int):
+    try:
+        conn = db_client()  
+        cur = conn.cursor()
+
+        cur.execute(
+            "UPDATE partida SET intents = intents + 1 WHERE partida_id = %s",
+            (partida_id,)
+        )
+        conn.commit()
+
+        cur.execute(
+            "SELECT intents FROM partida WHERE partida_id = %s",
+            (partida_id,)
+        )
+
+        intentos = cur.fetchone()
 
     finally:
         conn.close()
-    return frase
+
+    return intentos  
--- ACTIVITAT_11/main.py
+++ ACTIVITAT_11/main.py
@@ -1,25 +1,11 @@
-from connection import db_client
 from fastapi import FastAPI
-
+from db import intents, pillafrase
 app = FastAPI()
-
-def pillafrase(Idioma: str):
-    try:
-        conn = db_client() 
-        cur = conn.cursor()
-
-        cur.execute("SELECT frase FROM pantalla WHERE Idioma = %s", (Idioma,))
-        frase = cur.fetchall()  
-    finally:
-        conn.close()
-
-    return frase
 
 @app.get("/frase/{Idioma}")
 def show_alumne(Idioma: str):  
-    frase = pillafrase(Idioma)
+    return {"frase" :pillafrase(Idioma)}
 
-    if not frase:  
-        return {"message": "No hi ha frase"}
-
-    return {"frase": frase}
+@app.post("/partida/{partida_id}/fallo")
+def fallo_partida(partida_id: int):
+    return {"numero de intents": intents(partida_id)}
